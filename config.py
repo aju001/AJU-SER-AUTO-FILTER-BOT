4e6c7d3282fe24320097153e1c8fe04c8a7e88bc
Aju <94348613+aju001@users.noreply.github.com>
2022-08-07 22:40:16 +0530

    AJUSER💞

--- config.py
+++ config.py
@@ -12,25 +12,25 @@ def is_enabled(value, default):
 
 
 # Creator
-CREATOR_NAME = os.environ.get("CREATOR_NAME", "𝙼𝚄𝙷𝙰𝙼𝙼𝙴𝙳")
-CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "PR0FESS0_99")
+CREATOR_NAME = os.environ.get("CREATOR_NAME", "𝗮𝗷𝘂")
+CREATOR_USERNAME = os.environ.get("CREATOR_USERNAME", "ajukalex")
 
 # Account
 API_HASH = os.environ.get("API_HASH", "5b1d0992294a67cb54512a4fafeb0c88")
 API_ID = os.environ.get("API_ID", "6170803")
 # About Bot
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "1905228806:AAH2iahJcg5J6bqpcW11jB9KajwHbIjaslGTY")
-PICS = os.environ.get("PICS", "https://telegra.ph/file/034d53b5ed1d920ecab8b.jpg")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5515459324:AAFtLiYSASa5_Yi3PaEbXZxpXbcHWhzeEAI")
+PICS = os.environ.get("PICS", "https://i.ibb.co/kQ98t1k/AJU.jpg")
 # Database
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "LuciferMoringstar-Robot")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "AJU-SER-AUTO-FILTER-BOT")
 DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://{Username}:{Passs}@cluster0.{clusterID}.mongodb.net/myFirstDatabase?retryWrites=true{iD}=majority")
 # Chats & Users
-ADMINS = os.environ.get("ADMINS", "2028425293 1637186875")
+ADMINS = os.environ.get("ADMINS", "1389198873")
 SUPPORT_CHAT = os.environ.get("SUPPORT_CHAT", "Mo_Tech_Group")
 AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "-1001685151224")
-CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001784382279").split()]
+CHANNELS = [int(ch) if search.search(ch) else ch for ch in os.environ.get("CHANNELS", "-1001361180870").split()]
 LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "-1001590063851")
-GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001570208190")
+GET_FILECHANNEL = os.environ.get("GET_FILECHANNEL", "-1001421928092")
 FILTER_DEL_SECOND = int(os.environ.get("FILTER_DEL_SECOND", "600"))
 
 # AutoFilter
